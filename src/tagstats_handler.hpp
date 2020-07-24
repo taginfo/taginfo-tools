023f23ccc11a7802da4d7d65cc87ff72c6d9ac7b
Jochen Topf <jochen@topf.org>
2020-07-24 16:41:49 +0200

    Update copyright messages

--- src/tagstats_handler.hpp
+++ src/tagstats_handler.hpp
@@ -3,22 +3,22 @@
 
 /*
 
-  Copyright (C) 2012-2017 Jochen Topf <jochen@topf.org>.
+  Copyright (C) 2012-2020 Jochen Topf <jochen@topf.org>.
 
-  This file is part of Tagstats.
+  This file is part of Taginfo Tools.
 
-  Tagstats is free software: you can redistribute it and/or modify
+  Taginfo is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
-  Tagstats is distributed in the hope that it will be useful,
+  Taginfo is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with Tagstats.  If not, see <http://www.gnu.org/licenses/>.
+  along with Taginfo.  If not, see <http://www.gnu.org/licenses/>.
 
 */
 
